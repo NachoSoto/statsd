65e93793ca7638ce576c149b3059432718ec5936
NachoSoto <ignaciosoto90@gmail.com>
2012-12-17 02:01:29 +0000

    Added support to alter gauges based on previous value

--- stats.js
+++ stats.js
@@ -162,7 +162,11 @@ config.configFile(process.argv[2], function (config, oldConfig) {
             }
             timers[key].push(Number(fields[0] || 0));
           } else if (fields[1].trim() == "g") {
-            gauges[key] = Number(fields[0] || 0);
+            if (gauges[key] && fields[0].match(/^[-+]/)) {
+              gauges[key] += Number(fields[0] || 0);
+            } else {
+              gauges[key] = Number(fields[0] || 0);
+            }
           } else if (fields[1].trim() == "s") {
             if (! sets[key]) {
               sets[key] = new set.Set();
